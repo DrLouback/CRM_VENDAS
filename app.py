51dda77b2ef3f83d3650c61223e1fff221ff10be
DrLouback <drloubackmateus@gmail.com>
2024-09-12 19:40:01 -0300

    Criado contratos

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import streamlit as st
-from datetime import time
-import contrato
+from datetime import time,datetime
+from contrato import Vendas
 
 def main():
     
     st.title('Sistema de CRM')
     email = st.text_input('E-mail do vendedor')
-    data = st.date_input('Data da venda')
-    hora = st.time_input('Horário da venda')
+    data = st.date_input('Data da venda', datetime.now())
+    hora = st.time_input('Horário da venda', time(9,0))
     valor = st.number_input('Valor da venda', min_value=0.0, format="%0.2f")
-    quantidade = st.number_input('Quantidade de produtos')
+    quantidade = st.number_input('Quantidade de produtos', min_value= 0, step= 1 )
     produto = st.selectbox('Produto vendido',['ZapFlow com Gemini','ZapFlow com Lhama','ZapFlow com ChatGPT'])
+
     if st.button('Salvar'):
+        data_hora = datetime.combine(date= data, time= hora) #type: ignore
+        venda = Vendas(
+            email= email,
+            data= data_hora,
+            valor= valor,
+            quantidade= quantidade,
+            produto= produto
+        )
         st.write('Salvo no banco de dados')
 
 
 if __name__ == '__main__':
     main()
+
---
+++ contrato.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
+from datetime import datetime,time
+from typing import Tuple
+from enum import Enum
+
+
+class ProdutoEnum(str, Enum):
+    produto1 = 'ZapFlow com Gemini'
+    produto2 = 'ZapFlow com Lhama'
+    produto3 = 'ZapFlow com ChatGPT'
+
+
+class Vendas(BaseModel):
+    email: EmailStr
+    data: datetime
+    valor: PositiveFloat
+    quantidade: PositiveInt
+    produto: str
+
+    
